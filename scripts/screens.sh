a36f2ca2c0f6bd16cda0e71d534e762d7b93205c
ge3224 <ge3224@gmail.com>
2025-06-06 09:35:01 -0500

    fix: adjust config for second screen

--- scripts/screens.sh
+++ scripts/screens.sh
@@ -5,5 +5,6 @@ if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --mode 1600x900
 else
     xrandr --output "$intern" --primary --mode 1600x900
-    xrandr --output "$extern" --mode 3840x2160 --scale 0.75x0.75 --right-of "$intern" --auto
+    # xrandr --output "$extern" --mode 3840x2160 --scale 0.75x0.75 --right-of "$intern" --auto
+    xrandr --output "$extern" --mode 2560x1440 --right-of "$intern" --auto
 fi
